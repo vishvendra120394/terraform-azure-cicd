b297228f9a2f1251dddc8187ca19f78d97a8958e
Vishvendra Singh Chauhan <vishvendra.singh.chauhan@ericsson.com>
2026-08-15 20:38:31 +0530

    Configure Azure remote Terraform backend

--- env/pre_prod_env/provider.tf
+++ env/pre_prod_env/provider.tf
@@ -7,6 +7,13 @@ terraform {
 
     }
   }
+  backend "azurerm" {
+  resource_group_name  = "rg-terraform-state"
+  container_name       = "tfstate"
+  storage_account_name = "tfstatevish2026"
+  key                  = "pre-prod.terraform.tfstate"
+  use_azuread_auth     = true
+}
 }
 provider "azurerm" {
   features {
